d46361fd7e605df659d90fb7cca2f595f848ba75
Leo <losoviz@gmail.com>
2019-12-18 13:32:30 +0800

    Added TypeAPI

--- initialize.php
+++ initialize.php
@@ -1,1 +1,2 @@
 <?php
+\PoP\PagesWP\Component::init();
---
+++ src/TypeAPIs/PageTypeAPI.php
@@ -0,0 +1,21 @@
+<?php
+namespace PoP\PagesWP\TypeAPIs;
+
+use WP_Post;
+use PoP\Pages\TypeAPIs\PageTypeAPIInterface;
+/**
+ * Methods to interact with the Type, to be implemented by the underlying CMS
+ */
+class PageTypeAPI implements PageTypeAPIInterface
+{
+    /**
+     * Indicates if the passed object is of type Page
+     *
+     * @param [type] $object
+     * @return boolean
+     */
+    public function isInstanceOfPageType($object): bool
+    {
+        return ($object instanceof WP_Post) && $object->post_type == 'page';
+    }
+}
